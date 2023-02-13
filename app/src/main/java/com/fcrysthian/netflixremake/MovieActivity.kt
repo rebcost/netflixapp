045d696a1d042e0f56c388cf4fbd4bb7fd1edf6e
Felýpë Costa <rebelo.filipe020@gmail.com>
2023-02-13 16:40:43 -0400

    sincronizando scrollview com recyclerview

--- app/src/main/java/com/fcrysthian/netflixremake/MovieActivity.kt
+++ app/src/main/java/com/fcrysthian/netflixremake/MovieActivity.kt
@@ -1,23 +1,49 @@
 package com.fcrysthian.netflixremake
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fcrysthian.netflixremake.adapter.MovieAdapter
+import com.fcrysthian.netflixremake.model.Movie
 
 class MovieActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
+        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
+        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
+        val rv : RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        txtTitle.text = "Batman Begins"
+        txtDesc.text = "Essa é uma descrição do filme"
+        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Atriz A, Atriz B")
+
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 36){
+            val movie = Movie(R.drawable.movie)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
 
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
 
         // Busquei o desenhavel (layer-list)
         val layerDrawable : LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
